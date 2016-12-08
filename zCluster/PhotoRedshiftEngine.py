6a1f40d5af0dc6ac637579917f631984c4c1f07f
Matt Hilton <mattyowl@gmail.com>
2016-12-08 16:42:26 +0200

    Removed stray Sourcery references

--- zCluster/PhotoRedshiftEngine.py
+++ zCluster/PhotoRedshiftEngine.py
@@ -12,7 +12,7 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Sourcery is distributed in the hope that it will be useful,
+    zCluster is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
